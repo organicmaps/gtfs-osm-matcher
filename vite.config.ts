e5f3dac42c60411a27f783d278ae08f0d8f71630
Dmitry Kiselev <dmitry.v.kiselev@gmail.com>
2025-12-16 12:47:35 -0400

    define request paths for dev and prod (#5)

--- vite.config.ts
+++ vite.config.ts
@@ -2,26 +2,38 @@ import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [preact()],
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          preact: ['preact', 'preact/hooks', 'preact/compat'],
-          maplibre: ['maplibre-gl']
+export default defineConfig(({ mode }) => {
+  console.log("Build mode", mode);
+  const devBuild = mode === 'development';
+
+  const SchedulesAPIBase = devBuild ? "http://localhost:4567/v1/schedule" : "https://pt.organicmaps.app/api/v1/schedule";
+  const RTUpdatesAPIBase = devBuild ? "http://localhost:4567/v1/updates" : "https://pt.organicmaps.app/api/v1/updates";
+
+  return {
+    plugins: [preact()],
+    build: {
+      sourcemap: devBuild,
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            preact: ['preact', 'preact/hooks', 'preact/compat'],
+            maplibre: ['maplibre-gl']
+          }
         }
       }
-    }
-  },
-  server: {
-    proxy: {
-      '/data': {
-        target: 'http://localhost:8801',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/data/, ''),
-      }
     },
+    define: {
+      "SchedulesAPIBase": JSON.stringify(SchedulesAPIBase),
+      "RTUpdatesAPIBase": JSON.stringify(RTUpdatesAPIBase),
+    },
+    server: {
+      proxy: {
+        '/data': {
+          target: 'http://localhost:8801',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/data/, ''),
+        }
+      },
+    }
   }
-})
+});
